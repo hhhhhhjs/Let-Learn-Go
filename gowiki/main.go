e67d40107f2b3a2477b645110e21af640f3b3b0e
hhhhhhjs <1512403978@qq.com>
2025-06-27 17:29:40 +0800

    feat:使用 go 编写 web 应用程序

---
+++ gowiki/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+// 这里相当于 js 中的 class Page { save() {}}, save 就是类中内置的一个方法
+func (p *Page) save() error {
+	// p *Page 是方法的接收者
+	// 这里 需要使用指针 *Page ，就像 js 中引用数据类型引用的是对象的地址一样
+	// 如果使用 Page 则相当于拷贝了一份 Page, 修改之后并不会影响原对象
+	filename := p.Title + ".txt"
+	// 这里没有给文件路径，默认在文件的运行目录底下
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// 这里 w 作为第一个参数的作用是为了告诉编译器要通过 w 来将内容发送给浏览器
+	// w 是一个管道，stream
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+
+func main() {
+	    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+	// 这里相当于 new 一个实例
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	// 使用实例对象上的方法
+	// 写文件，创建一个名为 TestPage 的 txt 文本文件从、
+	p1.save()
+	// 读文件，读取文件中的内容
+	p2, _ := loadPage("TestPage")
+	// 目的是想要打印一下文件中的内容
+	fmt.Println(string(p2.Body))
+}
---
+++ gowiki/readme.md
@@ -0,0 +1,7 @@
+# go wiki
+## strcut 和 interface
+* struct 是 go 中的结构体，用来定义数据类型
+* interface 是 go 中的接口，用来定义方法类型
+* go 中的 interface 是一种类型，是一种方法签名的集合
+也就是说 struct 相当于前端中的对象或者类，可以定义存储数据的；
+但是 interface 只是用来抽象行为的
